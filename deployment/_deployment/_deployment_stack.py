f0e89ed7343db361291bd692a8b3593578c84414
Fernando Mora <f.mora.g90@gmail.com>
2022-02-10 13:14:59 -0300

    ci(cdk): use Secret instead of ISecret

--- deployment/_deployment/_deployment_stack.py
+++ deployment/_deployment/_deployment_stack.py
@@ -98,8 +98,8 @@ class MotivusMarketplaceApiStack(cdk.Stack):
             task_image_options=aws_ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
                 image=registry,
                 secrets={
-                    'DB_PASSWORD': aws_ecs.Secret.from_secrets_manager(db_password),
-                    'SECRET_KEY_BASE': aws_ecs.Secret.from_secrets_manager(secret_key)
+                    'DB_PASSWORD': db_password,
+                    'SECRET_KEY_BASE': secret_key
                 },
                 environment={
                     'MIX_ENV': 'prod',
